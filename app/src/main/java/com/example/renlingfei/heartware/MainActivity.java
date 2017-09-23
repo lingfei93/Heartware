33cd20e8fa9b50784b73c0a2d6f5f99648591efb
Lingfei <lren002@berkeley.edu>
2017-09-23 23:30:04 +0800

    first-commit for header

--- app/src/main/java/com/example/renlingfei/heartware/MainActivity.java
+++ app/src/main/java/com/example/renlingfei/heartware/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.renlingfei.heartware;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.GregorianCalendar;
+
 
 public class MainActivity extends AppCompatActivity {
+    MainActivity currentScreen;
+    TextView myTextView;
+    Button btn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //add comment
+        setContentView(R.layout.activity_bottom_nav);
     }
 }
