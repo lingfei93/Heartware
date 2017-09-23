d68d5eca4f747305280948cd9b1b5b9ec1293801
Lingfei <lren002@berkeley.edu>
2017-09-24 01:18:37 +0800

    test

--- app/src/main/java/com/example/renlingfei/heartware/MainActivity.java
+++ app/src/main/java/com/example/renlingfei/heartware/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.renlingfei.heartware;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.GregorianCalendar;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +27,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_nav);
+
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_profile:
+
+                        Toast.makeText(MainActivity.this, "Profidddle",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_homepage:
+                        break;
+                    case R.id.action_campaign:
+                        break;
+
+                }
+                return true;
+
+            }
+        });
     }
 }
