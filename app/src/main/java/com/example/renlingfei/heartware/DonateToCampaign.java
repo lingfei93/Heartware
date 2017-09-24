aecbe2c5161c3ef9b8f1993a24e34326e6e87127
jillianheng <jillian_2305@hotmail.com>
2017-09-24 13:33:01 +0800

    Pages

--- app/src/main/java/com/example/renlingfei/heartware/DonateToCampaign.java
+++ app/src/main/java/com/example/renlingfei/heartware/DonateToCampaign.java
@@ -1,19 +1,41 @@
 package com.example.renlingfei.heartware;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class DonateToCampaign extends AppCompatActivity {
+    Button backButton;
+    Button submitButton;
+    DonateToCampaign currentScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate_to_campaign);
 
+        backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(currentScreen,VolunteerForCampaign.class);
+                startActivity(myIntent);
+            }
+        });
+
+        submitButton = (Button) findViewById(R.id.donateCamButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(currentScreen, ProfilePage.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/renlingfei/heartware/VolunteerForCampaign.java
+++ app/src/main/java/com/example/renlingfei/heartware/VolunteerForCampaign.java
@@ -1,18 +1,51 @@
 package com.example.renlingfei.heartware;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class VolunteerForCampaign extends AppCompatActivity {
+    Button backButton;
+    Button donateToButton;
+    Button collectForButton;
+    VolunteerForCampaign currentScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_for_campaign);
+        currentScreen = this;
+        backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(currentScreen, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        donateToButton = (Button) findViewById(R.id.donateToButton);
+        donateToButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(currentScreen, DonateToCampaign.class);
+                startActivity(myIntent);
+            }
+        });
+
+        collectForButton = (Button) findViewById(R.id.collectForCamButton);
+        collectForButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(currentScreen, CollectForCampaign.class);
+                startActivity(myIntent);
+            }
+        });
 
     }
 
